5a4444c4039db4b319bb300fbb6fd3eec2b1bf57
Kurenai-Misuzu <61432427+Kurenai-Misuzu@users.noreply.github.com>
2024-01-18 15:24:44 -0800

    wrote javascript code for enabling and disabling the fieldsets

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DEV209_HW3
 Homework 3 for Dev209
+
+Kurenai-Misuzu - Dylan Verallo
--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    
+    // declare field variables
+    let newOrExistingField = document.querySelector("#newOrExistingField");
+    let newUserField = document.querySelector("#newUserField");
+    let existingUserField = document.querySelector("#existingUserField");
+
+    // disable the new user field and the existing user field
+    newUserField.disabled = true;
+    existingUserField.disabled = true;
 });
 
+// function called when "click for new user" is clicked
+function newUserClicked(){
+    // disables the first field and enables the new user field
+    newOrExistingField.disabled = true;
+    newUserField.disabled = false;
+}
+
+// function called when "click for existing user" is clicked
+function existingUserClicked(){
+    // disables the first field and enables the existing user field
+    newOrExistingField.disabled = true;
+    existingUserField.disabled = false;
+}
