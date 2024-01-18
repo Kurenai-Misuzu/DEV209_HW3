b41ab465f2b8fc5392b32b404ecb141059b96f6d
Kurenai-Misuzu <61432427+Kurenai-Misuzu@users.noreply.github.com>
2024-01-18 14:57:30 -0800

    wrote the outline for the HTML

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-
-    document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
-    });
+    
 });
 
