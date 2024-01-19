231bde69f103c17c4f06ef69dea1cfb8949e23fb
Kurenai-Misuzu <61432427+Kurenai-Misuzu@users.noreply.github.com>
2024-01-18 16:04:29 -0800

    finsihed javascript implementation

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // disable the new user field and the existing user field
     newUserField.disabled = true;
     existingUserField.disabled = true;
+
+
+
 });
 
 // function called when "click for new user" is clicked
@@ -21,4 +24,41 @@ function existingUserClicked(){
     // disables the first field and enables the existing user field
     newOrExistingField.disabled = true;
     existingUserField.disabled = false;
+}
+
+// function called when "sign up" is clicked
+function submitNewUser(){
+    // declare all the fields into it's own variable
+    let userName = document.querySelector("#newUserName").value;
+    let password = document.querySelector("#newPassword").value;
+    let city = document.querySelector("#newCity").value;
+    let state = document.querySelector("#newState").value;
+
+    // save variables into local storage
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("password", password);
+    localStorage.setItem("city", city);
+    localStorage.setItem("state", state);
+
+    // alert user upon successful login
+    alert(`Welcome ${userName}!`);
+}
+
+// function is called when "login" is clicked
+function submitExistingUser(){
+    // declare variables for user input username and password
+    let userInputUserName = document.querySelector("#oldUserName").value;
+    let userInputPassword = document.querySelector("#oldPassword").value;
+
+    // fetch the stored username and password from local storage
+    let currentUserName = localStorage.getItem("userName");
+    let currentPassword = localStorage.getItem("password");
+
+    // alert user if username and password is invalid if values don't match
+    // or if values do match, welcome the user.
+    if (userInputPassword === currentPassword && userInputUserName === currentUserName){
+        alert(`Welcome back ${currentUserName}!`);
+    } else {
+        alert("Invalid username or password!");
+    }
 }
